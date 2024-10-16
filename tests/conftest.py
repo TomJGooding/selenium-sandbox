8181b9059a850b6843007ceeb608edc535bf99c0
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2024-10-16 14:51:08 +0100

    add driver config file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,35 @@
+import json
+
 import pytest
 import selenium.webdriver
 
 
 @pytest.fixture
-def driver():
-    driver = selenium.webdriver.Chrome()
-    driver.implicitly_wait(0.5)
+def config(scope="session"):
+    with open("tests/config.json") as f:
+        config = json.load(f)
+
+    assert config["driver"] in ["Firefox", "Chrome", "Headless Chrome"]
+    assert isinstance(config["implicit_wait"], (int, float))
+    assert config["implicit_wait"] > 0
+
+    return config
+
+
+@pytest.fixture
+def driver(config):
+    if config["driver"] == "Firefox":
+        driver = selenium.webdriver.Firefox()
+    elif config["driver"] == "Chrome":
+        driver = selenium.webdriver.Chrome()
+    elif config["driver"] == "Headless Chrome":
+        opts = selenium.webdriver.ChromeOptions()
+        opts.add_argument("headless")
+        driver = selenium.webdriver.Chrome(options=opts)
+    else:
+        raise Exception(f"Driver '{config['driver']}' is not supported")
+
+    driver.implicitly_wait(config["implicit_wait"])
 
     yield driver
 
